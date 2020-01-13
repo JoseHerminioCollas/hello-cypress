74b376b221b6f7f72a8c65d54411ca5c9ded4d8c
Jose Collas <joseherminiocollas@goatstonc.com>
2020-01-13 08:31:20 -0800

    add drag to test

--- cypress/integration/map-lab-spec.js
+++ cypress/integration/map-lab-spec.js
@@ -1,15 +1,25 @@
 const latLn = String([47.60107032220255, -122.2991180419922])
 
+const urls = [
+  'https://map-lab-goatstone.appspot.com?debug=11',
+  'http://192.168.0.27:3000'
+]
+
 describe('Map Lab', () => {
   it('should have a debug DIV', function () {
-    cy.visit('http://192.168.0.27:3000')
-    cy.get('div.debug').contains('id')
-    cy.get('div.debug').find('div').should($div => {
-      const expectedLatLn = [47.01, -122.47].toString()
+    cy.visit(urls[1])
+    cy.get('div.debug').should($div => {
+      const expectedLatLn = String([47.01, -122.47])
       const debugTxt = $div.text()
-      const txt = debugTxt.replace(/center:(.{1,})zoom.*/, '$1')
-      // const txt = debugTxt.replace(/center:([/d|\.|,]{1,})/, '$1')
-      expect(expectedLatLn).to.equal(latLn)
+      const ltLn = debugTxt.replace(/center:(.{1,})zoom.*/, '$1')
+      expect(expectedLatLn).to.equal(ltLn)
+      cy.get('#map')
+        .trigger('mousedown', { which: 1 })
+        .trigger('mousemove', { clientX: 50, clientY: 50 })
+        .trigger('mouseup', { force: true })
+      expect(String([0, 0])).to.equal(
+        debugTxt.replace(/center:(.{1,})zoom.*/, '$1')
+      )
     })
   })
 })
